44c368be97b0d1f8de38e805693ce81d51da2b81
Javier Repi <xavirepi@hotmail.com>
2020-12-19 17:31:01 +0100

    finish iteration 6 - add points

--- js/src/game.js
+++ js/src/game.js
@@ -1,4 +1,5 @@
-const timeFPS = 60;
+const time_FPS = 60;
+const extraPoints_25SecBlock_FPS = 1500;
 
 class Game {
     constructor(ctx) {
@@ -13,7 +14,7 @@ class Game {
         this.fps = 1000 / 60;
 
         this.points = 0;
-        this.time = 100;
+        this.time = 30;
         this.timeCount = 0;
     }
 
@@ -28,35 +29,58 @@ class Game {
                 this.checkCollisions();
                 this.timeCount++;
 
-                if (this.timeCount % timeFPS === 0) {
+                if (this.timeCount % time_FPS === 0) {
                     this.time--;
+                    this.checkTime();
                 }
+
+                // For every 25 seconds alive the player gets 100 extra points - They could be added at the end of the game
+                if (this.timeCount % extraPoints_25SecBlock_FPS === 0) {
+                    this.points += 100;
+                }
+
+                this.gameWon();
 
             }, this.fps)
         }
 
     }
 
-    pause() {
+    gameWon() {
+        if (this.bubbles.length === 0) {
+            clearInterval(this.interval)
+            this.ctx.save();
+            this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
-    }
+            this.ctx.font = '50px Arial';
+            this.ctx.fillStyle = 'white';
+            this.ctx.textAlign = 'center';
+            this.ctx.fillText(
+                'YOU WON THE GAME!',
+                this.ctx.canvas.width / 2,
+                this.ctx.canvas.height / 2 - 30,
+            );
+            this.ctx.restore();
 
-    gameOver() {
-        clearInterval(this.interval);
+            setTimeout(() => {
+                this.ctx.save();
+                this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+                this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+          
+                this.ctx.font = '36px Arial';
+                this.ctx.fillStyle = 'white';
+                this.ctx.textAlign = 'center';
+                this.ctx.fillText(
+                  `Score: ${this.points}`,
+                  this.ctx.canvas.width / 2,
+                  this.ctx.canvas.height / 2 + 30,
+                );
+                this.ctx.restore();
+              }, 1500);
 
-        this.ctx.save();
-        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-
-        this.ctx.font = '36px Arial';
-        this.ctx.fillStyle = 'white';
-        this.ctx.textAlign = 'center';
-        this.ctx.fillText(
-            'GAME OVER!',
-            this.ctx.canvas.width / 2,
-            this.ctx.canvas.height / 2,
-        );
-        this.ctx.restore();
+              this.player.canFire = false;
+        }
     }
 
     clear() {
@@ -76,9 +100,26 @@ class Game {
         this.ctx.save();
         this.ctx.font = '30px Arial';
         this.ctx.fillStyle = 'white';
-        this.ctx.fillText(`TIME: ${this.time}`, this.ctx.canvas.width - 175, 50)
-        this.ctx.restore()
-    }75
+        this.ctx.fillText(`TIME: ${this.time}`, this.ctx.canvas.width - 175, 50);
+        this.ctx.restore();
+
+        // Final Countdown:
+        if (this.time == 3 || this.time == 2 || this.time == 1) {
+            this.ctx.save();
+            // this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            // this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+    
+            this.ctx.font = '100px Arial';
+            this.ctx.fillStyle = 'white';
+            this.ctx.textAlign = 'center';
+            this.ctx.fillText(
+                `${this.time}`,
+                this.ctx.canvas.width / 2,
+                this.ctx.canvas.height / 2,
+            );
+            this.ctx.restore();
+        }
+    }
 
     move() {
         this.player.move();
@@ -90,32 +131,28 @@ class Game {
     }
 
     splitBubble(bubble, idx) {
-        // La bola tocada por la bala tiene que ser la que se meta en este método y se divida/elimine
-        // Create 2 new balls half the size
-        // 2nd generation ball 1
         this.player.bullets = [];
-        if (bubble.r > 20) {
+        if (bubble.r >= 20) {
             this.bubbles.push(new Bubble(
-                ctx, 
+                ctx,
                 bubble.x,
                 bubble.y,
                 bubble.r / 2,
                 'pink',
                 -2,
                 -2
-                ));
-            // 2nd generation ball 2
+            ));
             this.bubbles.push(new Bubble(
-                ctx, 
+                ctx,
                 bubble.x,
                 bubble.y,
                 bubble.r / 2,
                 'blue',
                 2,
                 -2
-                ));
+            ));
         }
-        this.bubbles.splice(idx, idx+1);
+        this.bubbles.splice(idx, idx + 1);
         console.log(this.bubbles);
 
         // If ball.r < mainBall.r/5 --> Remove ball and don't create any more balls
@@ -136,13 +173,8 @@ class Game {
             const bulletCollides = this.player.bulletCollidesWith(bubble);
             if (bulletCollides) {
                 this.splitBubble(bubble, idx);
-                
-                this.player.canFire = false
-                setTimeout(() => {
-                    this.player.canFire = true;
-                }, 200);
 
-                if (bubble.r <= 25) {
+                if (bubble.r <= 20) {
                     console.log(bubble.r)
                     this.points += 100; // The smallest bubbles add +100 points
                 } else {
@@ -151,5 +183,51 @@ class Game {
             }
         });
     }
+
+    checkTime() {
+        if (this.time <= 0) {
+            return this.gameOver();
+        }
+    }
+
+    gameOver() {
+        clearInterval(this.interval);
+
+        this.ctx.save();
+        this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+        this.ctx.font = '36px Arial';
+        this.ctx.fillStyle = 'white';
+        this.ctx.textAlign = 'center';
+        this.ctx.fillText(
+            'GAME OVER!',
+            this.ctx.canvas.width / 2,
+            this.ctx.canvas.height / 2 - 30,
+        );
+        this.ctx.restore();
+
+        setTimeout(() => {
+            this.ctx.save();
+            this.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+            this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+      
+            this.ctx.font = '36px Arial';
+            this.ctx.fillStyle = 'white';
+            this.ctx.textAlign = 'center';
+            this.ctx.fillText(
+              `Final Score: ${this.points}`,
+              this.ctx.canvas.width / 2,
+              this.ctx.canvas.height / 2 + 30,
+            );
+            this.ctx.restore();
+          }, 1500);
+    }
+
+    // stopBullets() {
+    //     if (this.gameOver() || this.gameWon()) {
+    //         this.player.canFire = false;
+    //     }
+    // }
 
 }
